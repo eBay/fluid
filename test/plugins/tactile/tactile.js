59ad08fb07514744781989ced5acd8d4b8eb40b6
senthilp <senthil.padmanabhan@yahoo.com>
2014-02-26 16:14:17 -0800

    Adding license & test

---
+++ test/plugins/tactile/tactile.js
@@ -0,0 +1,59 @@
+(function() {
+    'use strict';
+
+    var assert = chai.assert;
+
+    it('Should be defined as a jQuery object', function() {
+      assert($(document.body).tact);
+    });
+
+    it('Should bind listeners properly', function() {
+      var evtObj,
+          noopCb;
+
+      // Noop functions
+      noopCb = function() {
+        return true;
+      };
+
+      $('body').tact({
+        'drag': noopCb,
+        'swipe': noopCb
+      });
+
+      evtObj = $._data($('body').get(0), 'events');
+
+      assert(evt['tact:drag'].length > 0);
+      assert(evt['tact:swipe'].length > 0);
+    });
+
+    it('Should disable listeners properly', function() {
+        var settingsObj;
+
+        $('body').tact('disable');
+
+        settingsObj = $('body').data('__TACTILE');
+
+        assert(settingsObj.enabled === 0);
+      });
+
+    it('Should enable listeners properly', function() {
+        var settingsObj;
+
+        $('body').tact('enable');
+
+        settingsObj = $('body').data('__TACTILE');
+
+        assert(settingsObj.enabled === 1);
+      });
+
+    it('Should destroy listeners properly', function() {
+        var settingsObj;
+
+        $('body').tact('enable');
+
+        settingsObj = $('body').data('__TACTILE');
+
+        assert(settingsObj === undefined);
+      });
+})();
